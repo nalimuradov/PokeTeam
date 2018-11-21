c1fb6900d2ddaa150c3f3d2462b5b45a3a2f1fa9
Nariman Alimuradov <nariman.alimuradov@gmail.com>
2018-11-21 01:38:18 -0500

    chains on skateboard p

--- node/app.js
+++ node/app.js
@@ -1,65 +1,13 @@
-var request = require('request');
+var http = require('http');
+var express = require('express');
 
-var getHoennDex = {
-	method: 'GET',
-	url: 'https://pokeapi.co/api/v2/pokedex/hoenn/'
-};
+const app = express();
+const port = 3000;
 
-var getFireTypes = {
-	method: 'GET',
-	url: 'https://pokeapi.co/api/v2/type/fire/'
-};
+app.set('view engine', 'ejs');
 
-function fireType(callback){
-	request(getFireTypes, function(err, resp, body){
-		var fireTypes = [];
-		if (!err){
-			var fire = JSON.parse(body);
-			for (i = 0; i < fire.pokemon.length; i++){
-				fireTypes[i] = fire.pokemon[i].pokemon.name;
-			}
-			callback(fireTypes);
-		}
-	})	
-}
-
-function hoenn(callback){
-	request(getHoennDex, function(err, resp, body){
-		var hoennDex = [];
-		if (!err){
-			var hoenn = JSON.parse(body);
-			for (i = 0; i < hoenn.pokemon_entries.length; i++){
-				hoennDex[i] = hoenn.pokemon_entries[i].pokemon_species.name;
-			}
-			callback(hoennDex);
-		}
-	});
-}
-
-var storeHoenn = [];
-var storeFire = [];
-
-hoenn(function(hoennDex){
-	fireType(function(fireTypes){
-		var totalArray = [];
-		storeFire = fireTypes;
-		storeHoenn = hoennDex;
-
-		for (i = 0; i < storeFire.length; i++){
-			for (j = 0; j < storeHoenn.length; j++){
-				if (storeFire[i] == storeHoenn[j]){
-					totalArray.push(storeFire[i]);
-				}
-			}
-		}
-		console.log(totalArray);
-	})
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.htm');
 });
 
-
-
-
-
-
-// get array of all hoenn pokemon
-// get array of all fire pokemon
+app.listen(port, () => console.log('listening'));
