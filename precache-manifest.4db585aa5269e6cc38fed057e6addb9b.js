a36d0f3594e9a2acf3b5bcd2a247e26343cd2d8a
Nariman Alimuradov <nariman.alimuradov@gmail.com>
2019-01-26 16:51:20 -0500

    Updates

---
+++ precache-manifest.4db585aa5269e6cc38fed057e6addb9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c0ba168957f05ec7f24",
+    "url": "/pkAPI/static/js/runtime~main.9c0ba168.js"
+  },
+  {
+    "revision": "277c9a2b90b849bf45e2",
+    "url": "/pkAPI/static/js/main.277c9a2b.chunk.js"
+  },
+  {
+    "revision": "1292ededc0846eca9090",
+    "url": "/pkAPI/static/js/1.1292eded.chunk.js"
+  },
+  {
+    "revision": "277c9a2b90b849bf45e2",
+    "url": "/pkAPI/static/css/main.211e4b7d.chunk.css"
+  },
+  {
+    "revision": "1292ededc0846eca9090",
+    "url": "/pkAPI/static/css/1.1f0cc4e8.chunk.css"
+  },
+  {
+    "revision": "8ff6a21775227213088b78259c6d6c45",
+    "url": "/pkAPI/index.html"
+  }
+];
